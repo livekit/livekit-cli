0e9b25b4b2ac6c063e9bb6835802c463aec08896
Mathew Kamkar <578302+matkam@users.noreply.github.com>
2022-10-14 17:25:17 -0700

    shell autocomplete functions (#107)
    * bash, zsh, fish autocomplete
    
    * linux install

--- install-cli.sh
+++ install-cli.sh
@@ -8,6 +8,9 @@ set -o pipefail
 
 REPO="livekit-cli"
 INSTALL_PATH="/usr/local/bin"
+BASH_COMPLETION_PATH="/usr/share/bash-completion/completions"
+ZSH_COMPLETION_PATH="/usr/share/zsh/site-functions"
+FISH_COMPLETION_PATH="/usr/share/fish/vendor_completions.d"
 
 log()  { printf "%b\n" "$*"; }
 abort() {
@@ -82,6 +85,28 @@ fi
 log "Installing ${REPO} ${VERSION}"
 log "Downloading from ${ARCHIVE_URL}..."
 
-curl -s -L "${ARCHIVE_URL}" | ${SUDO_PREFIX} tar xzf - -C "${INSTALL_PATH}" --wildcards --no-anchored "$REPO*"
+TEMP_DIR_PATH="$(mktemp -d)"
+
+curl -s -L "${ARCHIVE_URL}" | ${SUDO_PREFIX} tar xzf - -C "${TEMP_DIR_PATH}" --wildcards --no-anchored "$REPO*"
+
+mv "${TEMP_DIR_PATH}/livekit-cli" "${INSTALL_PATH}/livekit-cli"
+
+if [ -d "${TEMP_DIR_PATH}/autocomplete" ]
+then
+  if [ -d "${BASH_COMPLETION_PATH}" ]
+  then
+    mv "${TEMP_DIR_PATH}/autocomplete/bash_autocomplete" "${BASH_COMPLETION_PATH}/livekit-cli"
+  fi
+
+  if [ -d "${ZSH_COMPLETION_PATH}" ]
+  then
+    mv "${TEMP_DIR_PATH}/autocomplete/zsh_autocomplete" "${BASH_COMPLETION_PATH}/_livekit-cli"
+  fi
+
+  if [ -d "${FISH_COMPLETION_PATH}" ]
+  then
+    livekit-cli generate-fish-completion -o "${FISH_COMPLETION_PATH}/livekit-cli.fish"
+  fi
+fi
 
 log "\n$REPO is installed to $INSTALL_PATH\n"
