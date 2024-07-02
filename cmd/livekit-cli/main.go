f61667f890ac4408a3952f4fb35d58dee1565d0a
rektdeckard <friedtm@gmail.com>
2024-07-02 00:12:22 -0600

    chore(cmd): dummy binary prints to stderr and returns a nonzero code
    This way it will interrupt any scripts depending on the old name.

--- cmd/livekit-cli/main.go
+++ cmd/livekit-cli/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println(`The "livekit-cli" binary has been renamed to "lk". Try again with the new name!`)
+	fmt.Fprintln(os.Stderr, `The "livekit-cli" binary has been renamed to "lk". Try again with the new name!`)
+	os.Exit(1)
 }
