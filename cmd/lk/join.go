8482f34167e2892fe5ee13deba9d509a07ac2ad0
Denys Smirnov <dennwc@pm.me>
2024-08-20 14:28:10 +0300

    Send and receive DTMF in join command.

--- cmd/lk/join.go
+++ cmd/lk/join.go
@@ -30,10 +30,11 @@ import (
 	"github.com/pion/webrtc/v3"
 	"github.com/urfave/cli/v3"
 
-	provider2 "github.com/livekit/livekit-cli/pkg/provider"
 	"github.com/livekit/protocol/livekit"
 	"github.com/livekit/protocol/logger"
 	lksdk "github.com/livekit/server-sdk-go/v2"
+
+	provider2 "github.com/livekit/livekit-cli/pkg/provider"
 )
 
 var (
