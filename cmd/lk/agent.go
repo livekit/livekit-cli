e47f79f5f0550e051669691a5b9eee39cffc4562
Théo Monnom <theo.monnom@outlook.com>
2026-04-02 15:08:16 -0700

    improve agent detection error messages and consistency
    - Add friendly error when no agent project is detected (start/dev/console)
    - Print detected language and directory to stderr for start/dev/console
    - Deduplicate console detection logic to use shared detectProject()
    - Add nil guard in BytesToSamples for empty data

--- cmd/lk/agent.go
+++ cmd/lk/agent.go
@@ -562,7 +562,7 @@ func createAgent(ctx context.Context, cmd *cli.Command) error {
 
 	projectType, err := agentfs.DetectProjectType(os.DirFS(workingDir))
 	if err != nil {
-		return fmt.Errorf("unable to determine agent language: %w, please navigate to a directory containing an agent written in a supported language", err)
+		return noAgentError()
 	}
 	fmt.Printf("Detected agent language [%s]\n", util.Accented(string(projectType)))
 
@@ -749,7 +749,7 @@ func deployAgent(ctx context.Context, cmd *cli.Command) error {
 
 	projectType, err := agentfs.DetectProjectType(os.DirFS(workingDir))
 	if err != nil {
-		return fmt.Errorf("unable to determine agent language: %w, please make sure you are inside a directory containing an agent written in a supported language", err)
+		return noAgentError()
 	}
 	fmt.Printf("Detected agent language [%s]\n", util.Accented(string(projectType)))
 
@@ -1493,7 +1493,7 @@ func generateAgentDockerfile(ctx context.Context, cmd *cli.Command) error {
 
 	projectType, err := agentfs.DetectProjectType(os.DirFS(workingDir))
 	if err != nil {
-		return fmt.Errorf("unable to determine agent language: %w, please make sure you are inside a directory containing an agent written in a supported language", err)
+		return noAgentError()
 	}
 	fmt.Printf("Detected agent language [%s]\n", util.Accented(string(projectType)))
 
